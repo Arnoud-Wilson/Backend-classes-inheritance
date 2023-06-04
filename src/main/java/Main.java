6179eb0693374a1e072deab37ce9574a60fa882e
Arnoud-Wilson <arnoud608@gmail.com>
2023-06-04 08:19:53 +0200

    finished assignment four

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Customer customer = new Customer("Arnoud", "Wilson", new VisaCard(1));
-        Customer customer2 = new Customer("Marjolein", "Wilson-Baas", new MasterCard(2));
+        Customer customer = new Customer("Arnoud", "Wilson", new VisaCard(0));
+        Customer customer2 = new Customer("Marjolein", "Wilson-Baas", new MasterCard(0));
 
         customer.printName();
         customer2.printName();
 
         System.out.println(customer.getCreditCard().getDebt());
-        customer.getCreditCard().pay(5);
+        customer.getCreditCard().pay(100);
         System.out.println(customer.getCreditCard().getDebt());
+
+        System.out.println(customer2.getCreditCard().getDebt());
+        customer2.getCreditCard().pay(100);
+        System.out.println(customer2.getCreditCard().getDebt());
 
 
         }
--- src/main/java/VisaCard.java
+++ src/main/java/VisaCard.java
@@ -3,4 +3,9 @@ public class VisaCard extends CreditCard {
     public VisaCard(int debt) {
         super(debt);
     }
+
+    @Override
+    public void pay(int amount) {
+        super.pay(amount / 100 * 90) ;
+    }
 }
